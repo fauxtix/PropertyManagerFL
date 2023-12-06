33746d2a7911237e876eed21d096ed025c208c51
fauxtix <fauxtix.luix@hotmail.com>
2023-12-06 20:47:49 +0000

    Localization

--- PropertyManagerFL.Api/Constants/Authorization.cs
+++ PropertyManagerFL.Api/Constants/Authorization.cs
@@ -9,7 +9,7 @@
 
         public const string default_username = "fausto";
         public const string default_email = "fauxtix.luix@propertymanager.pt";
-        public const string default_password = "Admin123£.";
+        public const string default_password = "UWis#£.";
         public const Roles default_role = Roles.Administrator;
     }
 }
