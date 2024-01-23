2ffe525718a6e79686bd98ddb735f354615c08a8
fauxtix <fauxtix.luix@hotmail.com>
2024-01-23 16:48:34 +0000

    Add Logs to git

---
+++ PropertyManagerFL.Application/ViewModels/Logs/AppLogDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PropertyManagerFL.Application.ViewModels.Logs
+{
+    public class AppLogDto
+    {
+        public int Id { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string MessageTemplate { get; set; } = string.Empty;
+        public string Level { get; set; } = string.Empty;
+        public DateTime TimeStamp { get; set; }
+        public string Exception { get; set; } = string.Empty;
+        public string Properties { get; set; } = string.Empty;
+    }
+}
