d8a71e0501b39cd74c759b9bd8b14f7e96d14a40
fauxtix <fauxtix.luix@hotmail.com>
2024-01-23 15:44:43 +0000

    Logins_SQL

---
+++ PropertyManagerFL.Infrastructure/Database/Login_Api.sql
@@ -0,0 +1,7 @@
+USE [master]
+GO
+
+/****** Object:  Login [IIS APPPOOL\PropertyManagerAPI]    Script Date: 23/01/2024 15:43:13 ******/
+CREATE LOGIN [IIS APPPOOL\PropertyManagerAPI] FROM WINDOWS WITH DEFAULT_DATABASE=[PropertyManagerDB], DEFAULT_LANGUAGE=[us_english]
+GO
+
---
+++ PropertyManagerFL.Infrastructure/Database/Login_App.sql
@@ -0,0 +1,7 @@
+USE [master]
+GO
+
+/****** Object:  Login [IIS APPPOOL\PropertyManagerFL]    Script Date: 23/01/2024 15:43:39 ******/
+CREATE LOGIN [IIS APPPOOL\PropertyManagerFL] FROM WINDOWS WITH DEFAULT_DATABASE=[PropertyManagerDB], DEFAULT_LANGUAGE=[us_english]
+GO
+
